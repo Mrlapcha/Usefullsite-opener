5d09f13b4396a5f71ec4d1eee61390c6875b35ed
MrLapcha <rkhem1514@gmail.com>
2024-01-28 15:52:50 +0545

    comit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Button from './Button';
+
+const App = ()=> {
+    return <div>
+        <h1>Usefull Website</h1>
+        <h3>Which Website do u want to open?</h3>
+        <Button name="Google" link="https://google.com"/>
+        <Button name="Facebook" link="https://facebook.com"/>
+        <Button name="Youtube" link="https://youtube.com"/>
+        <br></br>
+        <Button name="Github" link="https://github.com "/>
+        <Button name="IOST" link="https://www.tuiost.edu.np"/>
+        
+    </div>
+}
+export default App;
